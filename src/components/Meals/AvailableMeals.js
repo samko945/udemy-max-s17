ed17e689609278117e8ad47aea982830c26048e2
Samuel <samkoj@hotmail.co.uk>
2021-08-08 14:20:27 +0100

    fetch meals from backend, show loading state

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,36 +1,44 @@
+import { useState, useEffect } from "react";
 import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
 import classes from "./AvailableMeals.module.css";
 
-const DUMMY_MEALS = [
-	{
-		id: "m1",
-		name: "Sushi",
-		description: "Finest fish and veggies",
-		price: 22.99,
-	},
-	{
-		id: "m2",
-		name: "Schnitzel",
-		description: "A german specialty!",
-		price: 16.5,
-	},
-	{
-		id: "m3",
-		name: "Barbecue Burger",
-		description: "American, raw, meaty",
-		price: 12.99,
-	},
-	{
-		id: "m4",
-		name: "Green Bowl",
-		description: "Healthy...and green...",
-		price: 18.99,
-	},
-];
-
 const AvailableMeals = () => {
-	const mealsList = DUMMY_MEALS.map((meal) => (
+	const [meals, setMeals] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
+
+	useEffect(() => {
+		const getMeals = async () => {
+			try {
+				const response = await fetch("https://udemy-max-s17-http-forms-default-rtdb.firebaseio.com/meals.json");
+				const data = await response.json();
+				const meals = [];
+				for (const key in data) {
+					meals.push({
+						id: data[key],
+						name: data[key].name,
+						description: data[key].description,
+						price: data[key].price,
+					});
+				}
+				setMeals(meals);
+				setIsLoading(false);
+			} catch (error) {
+				console.log(error);
+			}
+		};
+		getMeals();
+	}, []);
+
+	if (isLoading) {
+		return (
+			<section className={classes.MealsLoading}>
+				<p>Loading...</p>
+			</section>
+		);
+	}
+
+	const mealsList = meals.map((meal) => (
 		<MealItem key={meal.id} id={meal.id} name={meal.name} description={meal.description} price={meal.price} />
 	));
 
